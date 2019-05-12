749dad64e9d482c41e34420e78f37cfa61f00c82
AliGangeh <40417826+AliGangeh@users.noreply.github.com>
2019-05-11 18:03:58 -0700

    commented code

--- Multiclass_classification.py
+++ Multiclass_classification.py
@@ -1,3 +1,4 @@
+#import libraries
 import numpy as np
 import keras
 from sklearn import datasets
@@ -7,24 +8,30 @@ from keras.layers import Dense
 from keras.optimizers import Adam
 from keras.utils.np_utils import to_categorical
 
+#create 5 classes with 500 points each, store cordinates in X and their label in y
 n_pts = 500
 centers = [[-1, 1], [-1, -1], [1, -1], [1, 1], [0, 0]]
 X, y = datasets.make_blobs(n_samples=n_pts, random_state = 123, centers=centers, cluster_std=0.4)
 
+#plot the points on a graph
 plt.scatter(X[y==0, 0], X[y==0, 1])
 plt.scatter(X[y==1, 0], X[y==1, 1])
 plt.scatter(X[y==2, 0], X[y==2, 1])
 plt.scatter(X[y==3, 0], X[y==3, 1])
 plt.scatter(X[y==4, 0], X[y==4, 1])
 
-print(y)
+#this makes use of one-hot encoding instead of assigning a number like 0, 1, and 2 so the datasets
+#is not ordered it is not 0<1<2 this is unrelated, and one-hot allows that by assigning a binary
+#label like 100=0, 010=1, and 001=2 instead.
 y_cat = to_categorical(y, 5)
-print(y_cat)
+
+#creates model with keras
 model = Sequential()
 model.add(Dense(5, input_shape=(2,), activation='softmax'))
 model.compile(Adam(lr=0.1), 'categorical_crossentropy', metrics=['accuracy'])
-#one hot encode output
-history = model.fit(X, y_cat, verbose=1, batch_size = 50, epochs=100)
+history = model.fit(X, y_cat, verbose=1, batch_size = 50, epochs=10)
+
+#fuction for creating contours around classes
 def plot_multiclass_decision_boundary(X, y, model):
     x_span = np.linspace(min(X[:,0]) - 1, max(X[:,0]) + 1)
     y_span = np.linspace(min(X[:,1]) - 1, max(X[:,1]) + 1)
@@ -33,12 +40,8 @@ def plot_multiclass_decision_boundary(X, y, model):
     pred_func = model.predict_classes(grid)
     z = pred_func.reshape(xx.shape)
     plt.contourf(xx, yy, z)
-plot_multiclass_decision_boundary(X, y_cat, model)
-plt.scatter(X[y==0, 0], X[y==0, 1])
-plt.scatter(X[y==1, 0], X[y==1, 1])
-plt.scatter(X[y==2, 0], X[y==2, 1])
-plt.scatter(X[y==3, 0], X[y==3, 1])
-plt.scatter(X[y==4, 0], X[y==4, 1])
+
+#plots data with seperating contours
 plot_multiclass_decision_boundary(X, y_cat, model)
 plt.scatter(X[y==0, 0], X[y==0, 1])
 plt.scatter(X[y==1, 0], X[y==1, 1])
@@ -46,9 +49,9 @@ plt.scatter(X[y==2, 0], X[y==2, 1])
 plt.scatter(X[y==3, 0], X[y==3, 1])
 plt.scatter(X[y==4, 0], X[y==4, 1])
 
+#creates new point which is catagorized
 x = -0.5
 y = -0.5
-
 point = np.array([[x, y]])
 prediction = model.predict_classes(point)
 plt.plot([x], [y], marker='o', markersize=10, color="yellow")
